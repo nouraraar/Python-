ac3fc7ae0fc3087358e6519b4cdb3c716dbaa465
nouraraar <nourelhouda.araar@insat.u-carthage.tn>
2021-03-24 22:55:29 +0100

    added first.py

---
+++ intermediate python/matplotlib/first.py
@@ -0,0 +1,11 @@
+year=[1950,1970,1990,2010]
+pop=[2.519,3.692,5.263,6.972]
+#Print the last item from year and pop
+print(year[-1])
+print(pop[-1])
+#Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+#Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+#Display the plot with plt.show()
+plt.show()
